1553d9c5d93d5e7e928f120972053dc79f9d34c0
Nikita <belka.nik02@gmail.com>
2025-05-30 18:39:12 +0300

    page 404 complete, tablet and phone

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -9,17 +9,19 @@ export default function NotFoundPage() {
     navigate(-1);
   };
   return (
-    <div className={scss.backgroundBlock}>
-      <div className={scss.imageSnakes}>
-        <img src='/img/NotFoundPageImageSnakes.png' alt='Snakes' />
-      </div>
-      <div className={scss.textsBlock}>
-        <h3 className={scss.textError}>error</h3>
-        <h1 className={scss.textErrorNumb}>404</h1>
-        <p>the page was swallowed by monsters</p>
-        <a className={scss.buttonBack} onClick={handleGoBack}>
-          Back to WebSite <BsBoxArrowInRight className={scss.icon} />
-        </a>
+    <div className={scss.backgroundColor}>
+      <div className={scss.backgroundBlock}>
+        <div className={scss.imageSnakes}>
+          <img src='/img/NotFoundPageImageSnakes.png' alt='Snakes' />
+        </div>
+        <div className={scss.textsBlock}>
+          <h3 className={scss.textError}>error</h3>
+          <h1 className={scss.textErrorNumb}>404</h1>
+          <p>the page was swallowed by monsters</p>
+          <a className={scss.buttonBack} onClick={handleGoBack}>
+            Back to WebSite <BsBoxArrowInRight className={scss.icon} />
+          </a>
+        </div>
       </div>
     </div>
   );
