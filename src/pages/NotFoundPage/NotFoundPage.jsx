803e659245cca9628db61803d9e53a2c7923e2d8
Nikita <belka.nik02@gmail.com>
2025-05-30 16:07:53 +0300

    404 page pc version

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,3 +1,26 @@
+import scss from './NotFoundPage.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { BsBoxArrowInRight } from 'react-icons/bs';
+
 export default function NotFoundPage() {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+  return (
+    <div className={scss.backgroundBlock}>
+      <div className={scss.imageSnakes}>
+        <img src='/img/NotFoundPageImageSnakes.png' alt='Snakes' />
+      </div>
+      <div className={scss.textsBlock}>
+        <h3 className={scss.textError}>error</h3>
+        <h1 className={scss.textErrorNumb}>404</h1>
+        <p>the page was swallowed by monsters</p>
+        <a className={scss.buttonBack} onClick={handleGoBack}>
+          Back to WebSite <BsBoxArrowInRight className={scss.icon} />
+        </a>
+      </div>
+    </div>
+  );
 }
